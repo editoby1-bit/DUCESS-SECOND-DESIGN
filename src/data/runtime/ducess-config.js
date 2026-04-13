125cf5655dca8f02e806a38726ca127420d5ae68
editoby1-bit <editoby1@gmail.com>
2026-04-13 10:40:40 +0000

    Replace DUCESS frontend with updated build (runtime fix)

---
+++ src/data/runtime/ducess-config.js
@@ -0,0 +1,4 @@
+window.__DUCESS_CONFIG__ = window.__DUCESS_CONFIG__ || {
+  useSupabaseBackend: false,
+  storageKey: 'duces_enterprise_ledger_v1'
+};
---
+++ src/data/runtime/ducess-gateway.js
@@ -0,0 +1,5 @@
+window.DucessGateway = window.DucessGateway || {
+  createGateway() {
+    return null;
+  }
+};
